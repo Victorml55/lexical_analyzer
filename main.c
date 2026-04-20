4c8cd623e88ca6123d1bc259a4d5994591cb1c9e
Victorml55 <victorml5505@gmail.com>
2026-04-20 16:33:38 -0600

    metodo para obtener el arreglo de chars (#2)
    * add new file bhcdbde.txt
    
    * remove unused file bhcdbde.txt
    
    * Implementacion metodo get_file_content
    para pasar un archivo a un arreglo de caracteres

--- main.c
+++ main.c
@@ -2,12 +2,30 @@
 #include <stdlib.h>
 #include "include/list.h"
 
+char* get_file_content(const char* route) {
+    FILE* file = fopen(route, "rb"); //read binary para asignar el tamaño del archivo al buffer
+    if (!file) return NULL;
 
+    //calcular el tamaño del archivo moviendo el puntero al final
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file); //calcula la posicion del puntero y la asigna a size
+    rewind(file);
+
+    char* buffer = (char*)malloc(size + 1); //reserva de memoria
+    fread(buffer, 1, size, file);
+    buffer[size] = '\0';
+    
+    fclose(file);
+    return buffer;
+}
 
 int main(){
 	List* l = init_list();
 
 	print_list(l);
+
+	//prueba de lectura hermanito jeje
+	printf("%s\n", get_file_content("test2.ked"));
+
 	return 0;
 }
-
